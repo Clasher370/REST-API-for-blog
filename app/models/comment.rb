1d45275f04e5a604bc61d9ee9aa9b19523f12afb
tkasha <ivan.tkachef@gmail.com>
2017-07-06 17:37:40 +0300

    models, travis ci, env

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+
+  validates_presence_of :body
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+
+  validates_presence_of :nickname, :email, :password_digest
+end
